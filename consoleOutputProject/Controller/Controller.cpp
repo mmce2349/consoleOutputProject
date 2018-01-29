11a8b7417024b605f15bdeed8637ffbaa2e88b38
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2018-01-29 08:52:32 -0700

    Added some vectors and used them to make strings and ints.

--- consoleOutputProject/Controller/Controller.cpp
+++ consoleOutputProject/Controller/Controller.cpp
@@ -7,9 +7,28 @@
 //
 
 #include "Controller.hpp"
+#include <vector>
 using namespace std;
 Controller :: Controller()
 {
+    vector <int> firstAttempt;
+    vector <int> :: iterator i;
+    vector <int> :: reverse_iterator ir;
+    
+    for (int i =1; i<= 5; i++)
+        firstAttempt.push_back(i);
+    
+    
+    cout << "OutPut of begin and end\t:\t";
+    
+    vector <string> stringVector;
+    stringVector.push_back("monday");
+    stringVector.push_back("tuesday");
+   
+//    for(int i = 0; i < stringVector.length(); i++)
+//    {
+//        cout << stringVector[i] << endl;
+//    }
     
 }
 
